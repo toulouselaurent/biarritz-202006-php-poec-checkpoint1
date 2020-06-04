01f035298db324f878f1d23ee752d3eca1ac4ebe
blackshark <pierre.alexaline@gmail.com>
2020-06-04 21:51:03 +0200

    Checkpoint 1 - POEC Biarritz - may2020 - Master Subject

---
+++ README.md
@@ -0,0 +1,7 @@
+# PHP checkpoint 1
+
+* To run the checkpoint, you have to create a connec.php file 
+(you can use connec.php.dist as a model)
+* Launch the PHP internal server with `php -S localhost:8000 -t public`
+* Follow the instructions on the home page carefully.
+* Commit your changes on a branch named as **CAMPUS_NOM_Prenom** *(i.e. NANTES_LEGUENEC_Erwan)*
---
+++ public/book.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/book.css">
+    <title>Checkpoint PHP 1</title>
+</head>
+<body>
+
+<?php include 'header.php'; ?>
+
+<main class="container">
+
+    <section class="desktop">
+        <img src="image/whisky.png" alt="a whisky glass" class="whisky"/>
+        <img src="image/empty_whisky.png" alt="an empty whisky glass" class="empty-whisky"/>
+
+        <div class="pages">
+            <div class="page leftpage">
+                Add a bribe
+                <!-- TODO : Form -->
+            </div>
+
+            <div class="page rightpage">
+                <!-- TODO : Display bribes and total paiement -->
+            </div>
+        </div>
+        <img src="image/inkpen.png" alt="an ink pen" class="inkpen"/>
+    </section>
+</main>
+</body>
+</html>
